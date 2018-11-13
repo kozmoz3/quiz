8f4fbfc0b80e7b45cbfcaf765ee4519d2ca9b775
Enrique <kiqueyo847@gmail.com>
2018-11-13 09:27:22 -0600

    prueba 1

---
+++ quiz/src/main/groovy/com/quizwish/quiz/QuizApplication.groovy
@@ -0,0 +1,12 @@
+package com.quizwish.quiz
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class QuizApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run QuizApplication, args
+	}
+}
