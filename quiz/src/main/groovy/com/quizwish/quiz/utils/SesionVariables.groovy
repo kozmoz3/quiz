8959d75673ad360b4debcf518d1e1f08f5fcdd6b
kique6Delta <enrique.sosao@6delta.com>
2018-11-30 13:56:38 -0600

    probando rama

--- quiz/src/main/groovy/com/quizwish/quiz/utils/SesionVariables.groovy
+++ quiz/src/main/groovy/com/quizwish/quiz/utils/SesionVariables.groovy
@@ -4,14 +4,11 @@ import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
 
-import com.quizwish.quiz.sesion.AppUser
-
 
 class SesionVariables {
 
-	public static def sesionDetails() {
-		 AppUser user = (AppUser)SecurityContextHolder.getContext().getAuthentication().getPrincipal()
-return user;
-		 	//	return (principal instanceof UserDetails) ? (UserDetails) principal : null
+	public static UserDetails sesionDetails() {
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal()
+		return (principal instanceof UserDetails) ? (UserDetails) principal : null
 	}
 }
