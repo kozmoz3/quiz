5281d342101384d9163773657d8d251e7b6c4eab
AlfonsoAPI <alfonso.vasquez@apimicroservicios.com>
2018-12-05 14:57:41 -0600

    Modificando la vista principal y agregando tabla de precios

--- quiz/src/main/resources/static/home/js/active.js
+++ quiz/src/main/resources/static/home/js/active.js
@@ -1,16 +1,7 @@
 (function ($) {
     'use strict';
 
-    // :: 1.0 Owl Carousel Active Code
     if ($.fn.owlCarousel) {
-        /*$(".welcome_slides").owlCarousel({
-            items: 1,
-            loop: true,
-            autoplay: true,
-            smartSpeed: 1500,
-            nav: true,
-            navText: ["<i class='pe-7s-angle-left'</i>", "<i class='pe-7s-angle-right'</i>"]
-        });*/
         $(".app_screenshots_slides").owlCarousel({
             items: 1,
             loop: true,
@@ -32,53 +23,14 @@
             }
         });
     }
-
-    // :: 2.0 Slick Active Code
-    /*if ($.fn.slick) {
-        $('.slider-for').slick({
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            speed: 500,
-            arrows: false,
-            fade: true,
-            asNavFor: '.slider-nav'
-        });
-        $('.slider-nav').slick({
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            speed: 500,
-            asNavFor: '.slider-for',
-            dots: true,
-            centerMode: true,
-            focusOnSelect: true,
-            slide: 'div',
-            autoplay: true,
-            centerMode: true,
-            centerPadding: '30px',
-            mobileFirst: true,
-            prevArrow: '<i class="fa fa-angle-left"></i>',
-            nextArrow: '<i class="fa fa-angle-right"></i>'
-        });
-    }*/
-
-    // :: 3.0 Footer Reveal Active Code
-    /*if ($.fn.footerReveal) {
-        $('footer').footerReveal({
-            shadow: true,
-            shadowOpacity: 0.3,
-            zIndex: -101
-        });
-    }*/
-
-    // :: 4.0 ScrollUp Active Code
+    
     if ($.fn.scrollUp) {
         $.scrollUp({
             scrollSpeed: 1500,
             scrollText: '<i class="fa fa-angle-up"></i>'
         });
     }
-
-    // :: 6.0 onePageNav Active Code
+    
     if ($.fn.onePageNav) {
         $('#nav').onePageNav({
             currentClass: 'active',
@@ -96,8 +48,7 @@
     if ($window.width() > 767) {
         new WOW().init();
     }
-
-    // :: 8.0 Sticky Active Code
+    
     $window.on('scroll', function () {
         if ($window.scrollTop() > 48) {
             $('.header_area').addClass('sticky slideInDown');
