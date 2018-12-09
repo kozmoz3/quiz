f452570482a21a5677e8a80b3fd2b75c9d0e9eea
kique6Delta <enrique.sosao@6delta.com>
2018-12-08 23:22:58 -0600

    segurizando controlador

--- quiz/src/main/groovy/com/quizwish/quiz/controllers/UserController.groovy
+++ quiz/src/main/groovy/com/quizwish/quiz/controllers/UserController.groovy
@@ -2,6 +2,7 @@ package com.quizwish.quiz.controllers
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -19,7 +20,7 @@ class UserController {
 	@Autowired
 	@Qualifier("sessionUser")
 	SessionUser sessionUser;
-	
+	@PreAuthorize("hasRole('ROLE_ROOT')")
 	@GetMapping("/user")
 	def show(Model model) {
 		User user = sessionUser.userSessionAll();
