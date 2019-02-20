6e366821610d125b36401de3fdf61d097a8d45e7
Alfonso <alvaco_1993@hotmail.com>
2019-02-20 06:12:19 -0600

    Agregando vista de pagos

--- quiz/src/main/groovy/com/quizwish/quiz/controllers/InicioController.groovy
+++ quiz/src/main/groovy/com/quizwish/quiz/controllers/InicioController.groovy
@@ -1,5 +1,7 @@
 package com.quizwish.quiz.controllers
 
+import com.quizwish.quiz.component.SessionUser
+import com.quizwish.quiz.models.User
 import com.quizwish.quiz.models.jmodelos.MContacto
 import com.quizwish.quiz.services.EmailService
 
@@ -8,6 +10,7 @@ import org.apache.commons.logging.LogFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -25,10 +28,15 @@ class InicioController {
 	private static final Log LOGGER = LogFactory.getLog(InicioController.class)
 	
 	static final def INDEX = "index";
+	static final def DETAILS = "details";
 	
 	@Autowired
 	@Qualifier("servicioMails")
 	EmailService servicioMails;
+	
+	@Autowired
+	@Qualifier("sessionUser")
+	SessionUser sessionUser;
 	
 	@GetMapping("/")
 	public String showIndex(Model model ) {
@@ -40,5 +48,11 @@ class InicioController {
 	def sendEmail( @RequestBody MContacto contacto ) {
 		servicioMails.sendMail("alvaco_1993@hotmail.com", contacto.name + " > " + contacto.email, contacto.message)
 		return INDEX;
+	}
+	
+	@RequestMapping(value="/details", method = RequestMethod.GET)
+	def details(@AuthenticationPrincipal User principal, Model model) {
+		model.addAttribute("userlog", principal);
+		return DETAILS
 	}
 }
--- quiz/src/main/groovy/com/quizwish/quiz/entity/Estudiantequiz.groovy
+++ quiz/src/main/groovy/com/quizwish/quiz/entity/Estudiantequiz.groovy
@@ -6,6 +6,8 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 import javax.persistence.Table
 
 @Entity
@@ -28,6 +30,10 @@ class Estudiantequiz {
 	
 	@Column(name = "status")
 	boolean status
+	
+	@JoinColumn(name = "idquiz", referencedColumnName = "idquiz")
+	@ManyToOne(optional = false)
+	Quiz idquiz
 	
 	
 }
